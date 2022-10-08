f420b50eb64c83d3e1c181a2812f06405b569b8f
zile028 <zile028@gmail.com>
2022-10-08 19:03:24 +0200

    termin forma

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -3,26 +3,39 @@ const dbConfig = require("../config/dbConfig")
 const db = mongojs(dbConfig.CONNECTION_STRING, ["users"])
 
 const loginController = (req, res) => {
-	let {firstName, password} = req.body
-	db.users.findOne({firstName, password}, (err, docs) => {
-		console.log("login", docs)
-		if (err) {
-			res.redirect("/")
-		} else {
-			if (docs) {
-				req.session.user = docs
-				if (docs.role === "admin") {
-					res.redirect("/admin")
-				} else {
-					res.redirect("/")
-				}
-			} else {
-				res.redirect("/")
-			}
-		}
+    let {firstName, password} = req.body
+    db.users.findOne({firstName, password}, (err, docs) => {
+        console.log("login", docs)
+        if (err) {
+            res.redirect("/")
+        } else {
+            if (docs) {
+                req.session.user = docs
+                switch (docs.role) {
+                    case "admin" :
+                        res.redirect("/admin")
+                        break;
+                    case "operater":
+                        res.redirect("/operater")
+                        break;
+                    default:
+                        res.redirect("/")
+                }
 
 
-	})
+                // if (docs.role === "admin") {
+                // }else if (){
+                //
+                // }
+                // } else {
+
+            } else {
+                res.redirect("/")
+            }
+        }
+
+
+    })
 
 
 }
--- routs/index.js
+++ routs/index.js
@@ -4,6 +4,7 @@ const router = express.Router()
 router.use("/", require("./home"))
 router.use("/login", require("../controllers/loginController"))
 router.use("/admin", require("./admin"))
+router.use("/operater", require("./operater"))
 router.use("/logout", require("./logout"))
 
 module.exports = router
