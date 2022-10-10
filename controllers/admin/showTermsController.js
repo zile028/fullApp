3ed35e9f8e0e16305d1757085ccbb2fcca876d43
Zile028 <zile028@gmail.com>
2022-10-10 10:49:56 +0200

    switch to mongoose

--- controllers/admin/showTermsController.js
+++ controllers/admin/showTermsController.js
@@ -1,24 +1,14 @@
 const mongojs = require("mongojs");
-const { CONNECTION_STRING } = require("../../config/dbConfig");
+const {CONNECTION_STRING} = require("../../config/dbConfig");
 const db = mongojs(CONNECTION_STRING, ["terms"]);
 
 const showTermsController = (req, res, next) => {
-  let { id } = req.params;
-  db.users.findOne({ _id: mongojs.ObjectID(id) }, (err, user) => {
-    db.terms.find({ termsConsultant: id }, (err, terms) => {
-      res.render("admin/adminTerms", { terms, user });
-    });
-  });
+	let {id} = req.params;
+	db.users.findOne({_id: mongojs.ObjectID(id)}, (err, user) => {
+		db.terms.find({termsConsultant: id}, (err, terms) => {
+			res.render("admin/adminTerms", {terms, user});
+		});
+	});
 
-  let test = db.users.aggregate([
-    {
-      $lookup: {
-        from: "terms",
-        localField: "_id",
-        foreignField: "termsConsultant",
-        as: "consultantTerms",
-      },
-    },
-  ]);
 };
 module.exports = showTermsController;
--- routs/home.js
+++ routs/home.js
@@ -1,28 +1,31 @@
 const express = require("express")
 const router = express.Router()
+const mongojs = require("mongojs")
+const {CONNECTION_STRING} = require("../config/dbConfig")
+const db = mongojs(CONNECTION_STRING, ["users", "terms"])
 
 router.get("/", checkIsLogin, (req, res) => {
-    res.render("index")
+	res.render("index")
 })
 
 function checkIsLogin(req, res, next) {
-    if (req.session.user) {
-        switch (req.session.user.role) {
-            case "admin" :
-                res.redirect("/admin")
-                break;
-            case "operater":
-                res.redirect("/operator")
-                break;
-            case "savetnik":
-                res.redirect("/consultant")
-                break;
-            default:
-                res.redirect("/")
-        }
-    } else {
-        next()
-    }
+	if (req.session.user) {
+		switch (req.session.user.role) {
+			case "admin" :
+				res.redirect("/admin")
+				break;
+			case "operater":
+				res.redirect("/operator")
+				break;
+			case "savetnik":
+				res.redirect("/consultant")
+				break;
+			default:
+				res.redirect("/")
+		}
+	} else {
+		next()
+	}
 }
 
 
